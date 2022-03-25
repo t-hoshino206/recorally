5318b87c9618a04320768ad0d21c49e3d4ab9b21
t-hoshino206 <21st.century.eccentric.boy.f06@gmail.com>
2022-03-25 20:36:58 +0900

    Herokuデプロイ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  protect_from_forgery with: :exception
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER_2"] && password == ENV["BASIC_AUTH_PASSWORD_2"]
+    end
+  end
 end
